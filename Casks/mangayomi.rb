cb5fb4f069cf10acd7182897744f5d467ad11b32
Issac Lee <ill8266@rit.edu>
2025-05-13 15:22:57 -0400

    Update Cask

--- Casks/mangayomi.rb
+++ Casks/mangayomi.rb
@@ -1,11 +1,11 @@
 cask "mangayomi" do
-  version "0.6.0"
+  version "0.5.0"
   sha256 "78ef5888feaa38297431102adc740fd247afccf4dbc7f6f3d963ce003100ca9e"
 
   url "https://github.com/kodjodevf/mangayomi/releases/download/v#{version}/Mangayomi-v#{version}-macos.dmg"
   name "mangayomi"
-  desc ""
-  homepage ""
+  desc "Free and open source application for reading manga, novels, and watching anime"
+  homepage "https://github.com/kodjodevf/mangayomi"
 
   livecheck do
     url :url
