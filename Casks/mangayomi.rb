de6ded99a07d600589c7663195422583dd59e64f
Issac Lee <ill8266@rit.edu>
2025-05-31 04:25:39 +0000

    mangayomi 0.6.1

--- Casks/mangayomi.rb
+++ Casks/mangayomi.rb
@@ -1,6 +1,6 @@
 cask "mangayomi" do
-  version "0.6.0"
-  sha256 "78ef5888feaa38297431102adc740fd247afccf4dbc7f6f3d963ce003100ca9e"
+  version "0.6.1"
+  sha256 "3e81c5a6e047212e9645d9eb5095687e4b03eed0d369151fa670c9e90813eb72"
 
   url "https://github.com/kodjodevf/mangayomi/releases/download/v#{version}/Mangayomi-v#{version}-macos.dmg"
   name "mangayomi"
