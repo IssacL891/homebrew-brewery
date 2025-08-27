13da72274cb40ad6433dc6b76f5e0ff4c86fc032
Issac Lee <ill8266@rit.edu>
2025-08-27 16:03:56 -0400

    yaagl-hsr-os 0.2.27

--- Casks/yaagl-hsr-os.rb
+++ Casks/yaagl-hsr-os.rb
@@ -1,10 +1,10 @@
 cask "yaagl-hsr-os" do
-  version "0.2.26"
-  sha256 "ae8c566131165f8f4694a50aae84fe17e7ad5443d8b5da88cb565a9610d0dae5"
+  version "0.2.27"
+  sha256 "fb13922d31465c3267234e93c9134439ffef7ca961883c61a7abc96b55f700ad"
 
   url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.HSR.OS.app.tar.gz"
   name "Yet Another Anime Game Launcher"
-  desc "A launcher for an anime game overseas"
+  desc "Launcher for an anime game overseas"
   homepage "https://github.com/yaagl/yet-another-anime-game-launcher"
 
   livecheck do
@@ -13,9 +13,8 @@ cask "yaagl-hsr-os" do
   end
 
   depends_on macos: ">= :high_sierra"
+
   app "Yaagl HSR OS.app"
 
-  zap trash: [
-    "~/Library/Application Support/Yaagl HSR OS"
-]
+  zap trash: "~/Library/Application Support/Yaagl HSR OS"
 end
