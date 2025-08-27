094415726f6146bdf1d4f98116e2aac22938c1b4
Issac Lee <ill8266@rit.edu>
2025-08-27 15:40:16 -0400

    Attempt fix on adding new casks

--- Casks/yaggl-gi.rb
+++ Casks/yaggl-gi.rb
@@ -1,4 +1,4 @@
-cask "yaagl-gi" do
+cask "yaggl-gi" do
   version "0.2.26"
   sha256 "6a4117a8dbdae6a377ece0a2f493d9b544b7a9f32833e57a65f99734d1b2b8c4"
 
@@ -12,9 +12,9 @@ cask "yaagl-gi" do
     strategy :github_latest
   end
 
+  depends_on macos: ""
+
   app "Yaagl.app"
 
-  zap trash: [
-    "~/Library/Application Support/Yaagl"
-]
+  zap trash: "~/Library/Application Support/Yaagl"
 end
