4fe10183c5027bd49dc24a11f5243e8965ed091b
Issac Lee <ill8266@rit.edu>
2025-09-29 17:11:37 -0400

    fix url for dartotsu

--- Casks/Manga Readers/dartotsu.rb
+++ Casks/Manga Readers/dartotsu.rb
@@ -1,8 +1,7 @@
 cask "dartotsu" do
   version "0.0.3"
   sha256 "60e438d5b516836d37dd62a4fef1b7d2569d244dbe58ce222472d0da8c639205"
-
-  url "https://github.com/aayush2622/Dartotsu/releases/download/v#{version}/Dartotsu-macos-v#{version}.dmg"
+  url "https://github.com/aayush2622/Dartotsu/releases/download/v#{version}/Dartotsu-macos-vv#{version}.dmg"
   name "Dartotsu"
   desc " Dartotsu is a complete rewrite of Dantotsu in Flutter. It's a hybrid AniList and MyAnimeList tracking client, along with Simkl support!"
   homepage "https://github.com/aayush2622/Dartotsu"
