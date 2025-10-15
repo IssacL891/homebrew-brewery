d63a365df31b803b034eb232bad48750c5599abc
Issac Lee <ill8266@rit.edu>
2025-10-15 19:05:04 +0000

    anymex 3.0.2

--- Casks/Manga Readers/anymex.rb
+++ Casks/Manga Readers/anymex.rb
@@ -1,10 +1,10 @@
 cask "anymex" do
-  version "3.0.1"
-  sha256 "7b79be1e43a7f8c524efbc1a68f0689be539a1a1c47314bccfbb8f249a6596cc"
+  version "3.0.2"
+  sha256 "5107d5563545c9bf3dd9566486314b23cd7f71c5563461dc4fd81e7b02c02319"
 
   url "https://github.com/RyanYuuki/AnymeX/releases/download/v#{version}/AnymeX.dmg"
   name "AnymeX"
-  desc "A multiservice tracking client for anime and managa progress"
+  desc "Multiservice tracking client for anime and managa progress"
   homepage "https://github.com/RyanYuuki/AnymeX"
 
   livecheck do
