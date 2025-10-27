60d3c5185b930670932b183df29d96d24da5dc3c
Issac Lee <ill8266@rit.edu>
2025-10-27 19:05:41 +0000

    yaagl-hsr-os 0.3.0

--- Casks/Yaagl/yaagl-hsr-os.rb
+++ Casks/Yaagl/yaagl-hsr-os.rb
@@ -1,6 +1,6 @@
 cask "yaagl-hsr-os" do
-  version "0.2.30"
-  sha256 "92ec83a2fb77692bffeb95c79048286aa21c0cc7959bc42957efb718a8b5bf4a"
+  version "0.3.0"
+  sha256 "b4d68e9724ec1cb3447b769249339e0154bfb1442578b0f98c14e6d79d818878"
 
   url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.HSR.OS.app.tar.gz"
   name "Yet Another Anime Game Launcher"
