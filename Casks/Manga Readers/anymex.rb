78dd776d3e3188a2c2d7c57304dd8bebbc2c2055
Issac Lee <ill8266@rit.edu>
2025-11-10 04:29:23 +0000

    anymex 3.0.3

--- Casks/Manga Readers/anymex.rb
+++ Casks/Manga Readers/anymex.rb
@@ -1,6 +1,6 @@
 cask "anymex" do
-  version "3.0.2"
-  sha256 "5107d5563545c9bf3dd9566486314b23cd7f71c5563461dc4fd81e7b02c02319"
+  version "3.0.3"
+  sha256 "882232e5e37eddf2169c924dea5ff33fa9732aba30ee8034f59cfe12a45ce608"
 
   url "https://github.com/RyanYuuki/AnymeX/releases/download/v#{version}/AnymeX.dmg"
   name "AnymeX"
