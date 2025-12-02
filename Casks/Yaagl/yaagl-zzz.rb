9976942b5d00f6762de4c75db7ee63c3f9c00799
Issac Lee <ill8266@rit.edu>
2025-12-02 16:34:01 +0000

    yaagl-zzz 0.3.3

--- Casks/Yaagl/yaagl-zzz.rb
+++ Casks/Yaagl/yaagl-zzz.rb
@@ -1,6 +1,6 @@
 cask "yaagl-zzz" do
-  version "0.3.2"
-  sha256 "4fda6c23b6985e21d5dcf444aab781d03742250e372c08b319d472ee7bbddcab"
+  version "0.3.3"
+  sha256 "b56993c483c711c7b9c8407d7b09e1b71c58ebd3f46e9f904422d1f6d03a5f4c"
 
   url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.ZZZ.app.tar.gz"
   name "Yet Another Anime Game Launcher"
