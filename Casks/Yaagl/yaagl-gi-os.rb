b2d9f870c1d50b98e410143fd9110460a3fa3bc8
Issac Lee <ill8266@rit.edu>
2026-01-23 16:34:57 +0000

    yaagl-gi-os 0.3.7

--- Casks/Yaagl/yaagl-gi-os.rb
+++ Casks/Yaagl/yaagl-gi-os.rb
@@ -1,6 +1,6 @@
 cask "yaagl-gi-os" do
-  version "0.3.6"
-  sha256 "8087daae6ebf9ecffbc73d7375f02772d113c29846f2c3000541bab7d83948d2"
+  version "0.3.7"
+  sha256 "eeb1c598f8cb85b66f60dc26b3591a9b70e905cabef0f42cb47e36f9f6ce3d28"
 
   url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.OS.app.tar.gz"
   name "Yet Another Anime Game Launcher"
